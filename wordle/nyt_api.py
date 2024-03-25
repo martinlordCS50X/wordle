b6c0b817467efc70657d8c0d930053ac705f7e34
martinlordCS50X <140948825+martinlordCS50X@users.noreply.github.com>
2024-03-25 08:59:01 -0400

    Update nyt_api.py

--- wordle/nyt_api.py
+++ wordle/nyt_api.py
@@ -1,10 +1,18 @@
 import datetime
 import requests
 
-date = datetime.date.today()
-url = f"https://www.nytimes.com/svc/wordle/v2/{date:%Y-%m-%d}.json"
-response = requests.get(url).json()
-answer = response["solution"]
 
-if __name__ == "__name__":
+# Function to fetch Wordle answer for a given date
+def get_wordle_answer(date):
+    url = f"https://www.nytimes.com/svc/wordle/v2/{date}.json"
+    response = requests.get(url).json()
+    return response["solution"]
+
+
+# Instantiate answer variable with today's Wordle answer
+today_date = datetime.date.today()
+answer = get_wordle_answer(today_date)
+
+# Print today's Wordle answer if the script is run directly
+if __name__ == "__main__":
     print(f"Today's NYT Wordle answer is '{answer}'")
